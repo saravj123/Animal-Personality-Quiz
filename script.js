4b7f0d142c30038b3b3da58d24c84f327470eec6
saravj123 <86685070+saravj123@users.noreply.github.com>
2026-04-03 19:15:58 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+let cat = 0;
+let dog = 0;
+let fox = 0;
+let owl = 0;
+
+function choose(type) {
+
+    if (type == "cat") {
+        cat = cat + 1;
+    } else if (type == "dog") {
+        dog = dog + 1;
+    } else if (type == "fox") {
+        fox = fox + 1;
+    } else {
+        owl = owl + 1;
+    }
+
+}
+
+function result() {
+
+    let max = cat;
+    let animal = "Cat";
+
+    if (dog > max) {
+        max = dog;
+        animal = "Dog";
+    }
+
+    if (fox > max) {
+        max = fox;
+        animal = "Fox";
+    }
+
+    if (owl > max) {
+        max = owl;
+        animal = "owl";
+    }
+
+    document.getElementById("output").textContent =
+        "You are a " + animal;
+
+}
